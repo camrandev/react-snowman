3722457f74ada89f9cfc46412df69c3c7f17ce4e
Crynowecer0 <crynowecer11@gmail.com>
2023-05-24 19:08:08 -0400

    complete steps 2 and 3

--- src/Snowman.js
+++ src/Snowman.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import WrongGuesses from './WrongGuesses.js'
+import WrongGuesses from "./WrongGuesses.js";
+import { randomWord, ENGLISH_WORDS } from "./words.js";
 
 import "./Snowman.css";
 import img0 from "./0.png";
@@ -9,7 +10,6 @@ import img3 from "./3.png";
 import img4 from "./4.png";
 import img5 from "./5.png";
 import img6 from "./6.png";
-
 
 /** Snowman game: plays hangman-style game with a melting snowman.
  *
@@ -25,23 +25,22 @@ import img6 from "./6.png";
  */
 
 function Snowman({
-      images=[img0, img1, img2, img3, img4, img5, img6],
-      words=["apple"],
-      maxWrong=6,
-    }) {
+  images = [img0, img1, img2, img3, img4, img5, img6],
+  words = ENGLISH_WORDS,
+  maxWrong = 6,
+}) {
   /** by default, allow 6 guesses and use provided gallows images. */
 
   const [nWrong, setNWrong] = useState(0);
   const [guessedLetters, setGuessedLetters] = useState(() => new Set());
-  const [answer, setAnswer] = useState((words)[0]);
+  //why would this not work with passing ENGLISH_WORDS directly?
+  const [answer, setAnswer] = useState(randomWord(words));
 
   /** guessedWord: show current-state of word:
    if guessed letters are {a,p,e}, show "app_e" for "apple"
    */
   function guessedWord() {
-    return answer
-        .split("")
-        .map(ltr => (guessedLetters.has(ltr) ? ltr : "_"));
+    return answer.split("").map((ltr) => (guessedLetters.has(ltr) ? ltr : "_"));
   }
 
   /** handleGuess: handle a guessed letter:
@@ -51,40 +50,46 @@ function Snowman({
   function handleGuess(evt) {
     let ltr = evt.target.value;
 
-    setGuessedLetters(g => {
-      console.log(g)
+    setGuessedLetters((g) => {
+      console.log(g);
       const newGuessed = new Set(g);
       newGuessed.add(ltr);
       return newGuessed;
     });
 
-    setNWrong(n => n + (answer.includes(ltr) ? 0 : 1));
+    setNWrong((n) => n + (answer.includes(ltr) ? 0 : 1));
   }
 
   /** generateButtons: return array of letter buttons to render */
   function generateButtons() {
-    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
-        <button
-            key={ltr}
-            value={ltr}
-            onClick={handleGuess}
-            disabled={guessedLetters.has(ltr)}
-        >
-          {ltr}
-        </button>
+    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr) => (
+      <button
+        key={ltr}
+        value={ltr}
+        onClick={handleGuess}
+        disabled={guessedLetters.has(ltr)}
+      >
+        {ltr}
+      </button>
     ));
   }
 
   return (
-      <div className="Snowman">
-        <img src={(images)[nWrong]} alt={nWrong} />
-        <WrongGuesses wrong={nWrong} />
-        <p className="Snowman-word">{guessedWord()}</p>
+    <div className="Snowman">
+      <img src={images[nWrong]} alt={nWrong} />
+      <WrongGuesses wrong={nWrong} />
+      <p className="Snowman-word">{guessedWord()}</p>
+      {nWrong < maxWrong && (
         <p className="Snowman-buttons">{generateButtons()}</p>
-        <p style={{display: "none"}}>test</p>
-      </div>
+      )}
+      {nWrong === maxWrong && (
+        <div className="lose">
+          <p>you lose</p>
+          <p>the correct word is: {answer}</p>
+        </div>
+      )}
+    </div>
   );
 }
-
 
 export default Snowman;
--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -3,27 +3,29 @@ import Snowman from "./Snowman";
 import TEST_IMAGES from "./_testCommon.js";
 
 it("renders snowman without crashing", function () {
-  render(<Snowman
-    images={TEST_IMAGES}
-    words={["test"]}
-    maxWrong={3}
-    />);
+  render(<Snowman images={TEST_IMAGES} words={["test"]} maxWrong={3} />);
+});
+
+it("matches snapshot", function () {
+  const { container } = render(
+    <Snowman images={TEST_IMAGES} words={["test"]} maxWrong={3} />
+  );
+
+  expect(container).toMatchSnapshot();
 });
 
 it("stops game after max number of wrong guesses", function () {
-  const { container } = render(<Snowman
-    images={TEST_IMAGES}
-    words={["test"]}
-    maxWrong={3}
-    />);
+  const { container } = render(
+    <Snowman images={TEST_IMAGES} words={["test"]} maxWrong={3} />
+  );
 
   const a = container.querySelector("button[value=a]");
   fireEvent.click(a);
-  const b = container.querySelector("button[value=a]");
+  const b = container.querySelector("button[value=b]");
   fireEvent.click(b);
-  const c = container.querySelector("button[value=a]");
+  const c = container.querySelector("button[value=c]");
   fireEvent.click(c);
 
   expect(container.querySelector(".lose")).toBeInTheDocument();
   expect(container.querySelector(".Snowman-buttons")).not.toBeInTheDocument();
-})
+});
